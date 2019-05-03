463629e648351efb34ff51dbb88491cc9a6421d8
matusca96 <gomesloud@gmail.com>
2019-05-03 10:51:55 -0300

    Atualização.

--- webapp/controller/LoteArmazenagem.controller.js
+++ webapp/controller/LoteArmazenagem.controller.js
@@ -15,19 +15,24 @@ sap.ui.define([
 			this.getOwnerComponent().setModel(oParamModel, "parametros");
 			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
 			
-			this.getModel().attachMetadataLoaded(function(){
-				var oFilter = new Filter("Empresa", FilterOperator.EQ, Session.get("EMPRESA_ID"));
-				var oView = this.getView();
-				var oTable = oView.byId("tableLote");
-				var oColumn = oView.byId("columnDescricao");
-				
-				oTable.sort(oColumn);
-				oView.byId("tableLote").getBinding("rows").filter(oFilter, "Application");
+			var oFilter = new Filter("Empresa", FilterOperator.EQ, Session.get("EMPRESA_ID"));
+			var oView = this.getView();
+			var oTable = oView.byId("tableLote");
+			
+			oTable.bindRows({
+				path: '/LoteArmazenagems',
+				sorter: {
+					path: 'Descricao'
+				},
+				parameters: {
+					expand: 'LocalEstoqueDetails,ParceiroNegocioDetails,ProdutoDetails,TabelaTarifaDetails'
+				},
+				filters: oFilter
 			});
 		},
 		
 		filtraLote: function(oEvent){
-			var sQuery = oEvent.getParameter("query");
+			var sQuery = oEvent.getParameter("query").toUpperCase();
 			var oFilter1 = new Filter("Empresa", FilterOperator.EQ, Session.get("EMPRESA_ID"));
 			var oFilter2 = new Filter("Descricao", FilterOperator.Contains, sQuery);
 			
